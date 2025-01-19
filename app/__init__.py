d20c489fd626ad99050760098803b485d3216f77
odisfm <102568741+odisfm@users.noreply.github.com>
2025-01-19 15:51:28 +1100

    refactor: remove unused code

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,5 @@
 from .zcx_core import ZCXCore, Specification
 
-# logger = None
-
 
 def create_instance(c_instance):
-    # global logger
-    # logger = logging.getLogger(__name__)
     return ZCXCore(Specification, c_instance=c_instance)
