b9bf98ac453e46b70183445bc0ba6d5148059d74
odisfm <102568741+odisfm@users.noreply.github.com>
2025-01-21 10:04:18 +1100

    feat: add @properties

--- app/pad_section.py
+++ app/pad_section.py
@@ -14,9 +14,26 @@ class PadSection(EventObject):
             width
     ):
         super().__init__()
+        self.__pages_in = pages_in
+        self.__owned_coordinates = owned_coordinates
+        self.__width = width
+        self.__in_view = False
         self._logger = self.page_manager._logger.getChild(f'matrix_section__{section_name}')
         self.log(f'I appear in pages {pages_in}')
 
     def log(self, *msg):
         for msg in msg:
             self._logger.info(msg)
+
+    @property
+    def owned_coordinates(self):
+        return self.__owned_coordinates
+
+    @property
+    def width(self):
+        return self.__width
+
+    @property
+    def in_view(self):
+        return self.__in_view
+
