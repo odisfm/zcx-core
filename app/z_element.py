78149e7302be94c685d1d570ea07509b9ca6b0b8
odisfm <102568741+odisfm@users.noreply.github.com>
2025-01-22 09:47:39 +1100

    fix: constructor method signature

--- app/z_element.py
+++ app/z_element.py
@@ -3,6 +3,7 @@ from ableton.v3.control_surface.elements import ButtonElement
 
 class ZElement(ButtonElement):
 
-    def __init__(self):
-        super(ZElement, self).__init__()
+    def __init__(self, *a, **k):
+        super(ZElement, self).__init__(*a, **k)
         self.__name = 'unnamed_z_element'
+
