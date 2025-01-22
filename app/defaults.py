e401869e8535fca072308258a45a2e15588321f3
odisfm <102568741+odisfm@users.noreply.github.com>
2025-01-22 15:39:45 +1100

    feat: add defaults.py

---
+++ app/defaults.py
@@ -0,0 +1,49 @@
+BUILT_IN_COLORS = [
+    '__rgb__animate_success__',
+    '__rgb__animate_failure__',
+    '__rgb__attention__',
+    '__rgb__blank__',
+    '__basic__animate_success__',
+    '__basic__animate_failure__',
+]
+
+class DefaultColors:
+
+    __rgb__animate_success__ = {
+        "blink": {
+            "a": "base",
+            "b": "white",
+            "speed": 3
+        }
+    }
+
+    __rgb__animate_failure__ = {
+        "blink": {
+            "a": "base",
+            "b": "red",
+            "speed": 4
+        }
+    }
+
+    __rgb__attention__ = {
+        "pulse": {
+            "a": "base",
+            "b": "white"
+        }
+    }
+
+    __rgb__blank__ = {
+        "midi": 1
+    }
+
+    __basic__animate_success__ = {
+        "blink": {
+            "speed": 2
+        }
+    }
+
+    __basic__animate_failure__ = {
+        "blink": {
+            "speed": 5
+        }
+    }
