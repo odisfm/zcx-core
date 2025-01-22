9c8366766af28e8ab923dafbe54d6f5f492467f4
odisfm <102568741+odisfm@users.noreply.github.com>
2025-01-22 19:07:25 +1100

    feat: add consts

--- app/consts.py
+++ app/consts.py
@@ -3,3 +3,16 @@ APP_NAME = "zcx-core"
 REQUIRED_HARDWARE_SPECS = ["cc_buttons", "note_buttons", "encoders"]
 
 CXP_NAME = 'ClyphX_Pro'
+
+SUPPORTED_GESTURES = [
+    'pressed',
+    'pressed_delayed',
+    'released',
+    'released_immediately',
+    'released_delayed',
+    'double_clicked'
+]
+
+TEMPLATE_IDENTIFIER = '@'
+
+LIVE_TEMPLATE_IDENTIFIER = '$'
