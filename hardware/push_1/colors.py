954eda8b8ad460541de91593c7ec59398b9f2559
odisfm <102568741+odisfm@users.noreply.github.com>
2025-01-22 17:08:43 +1100

    feat: add convenience methods

--- hardware/push_1/colors.py
+++ hardware/push_1/colors.py
@@ -7,6 +7,18 @@ Blink,
 RgbColor
 )
 
+white = RgbColor(3)
+green = RgbColor(22)
+pulse_test = Pulse(white, green, 48)
+
+animation_speed_translation = [48, 24, 12, 8, 4]
+
+def translate_speed(speed):
+    if speed < 1 or speed >= len(animation_speed_translation):
+        # raise ConfigurationError(f'Invalid speed: {speed}')
+        return 48
+    return animation_speed_translation[speed]
+
 class BasicColorSwatch(Basic):
 
     base = RgbColor(127)
@@ -19,3 +31,7 @@ class RgbColorSwatch(Rgb):
 
     base = Rgb.MAGENTA
 
+def simplify_color(color):
+    from .. import ROOT_LOGGER
+    color1 = getattr(color, 'color1', color)
+    return color1
