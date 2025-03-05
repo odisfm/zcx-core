2b6ed34ec053c8e58d9567fe003b8805912fd7f0
odisfm <102568741+odisfm@users.noreply.github.com>
2025-03-05 18:48:24 +1100

    feat: listen to push main encoders

--- hardware/push_1/plugins/push_1_display/push_1_display.py
+++ hardware/push_1/plugins/push_1_display/push_1_display.py
@@ -1,10 +1,14 @@
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Optional
 
+from ableton.v3.base import listens_group
 from Push.sysex import *
 
 if TYPE_CHECKING:
     from app.zcx_plugin import ZCXPlugin
+    from app.z_encoder import ZEncoder
+    from app.encoder_manager import EncoderManager
 
+ENC_PREFIX = 'enc_'
 
 class Push1Display(ZCXPlugin):
 
@@ -17,9 +21,19 @@ class Push1Display(ZCXPlugin):
         super().__init__(name, *a, **k)
         self.set_logger_level('debug')
         self.__send_midi = self.canonical_parent._do_send_midi
+        self.__push_main_encoders: 'Optional[list[ZEncoder]]' = [None]*8
+        self.__encoder_manager: 'EncoderManager' = None
 
     def setup(self):
         super().setup()
+        self.__encoder_manager = self.component_map['EncoderManager']
+        for i in range(8):
+            enc_name = f'{ENC_PREFIX}{i+1}'
+            enc_obj = self.__encoder_manager.get_encoder(enc_name)
+            self.__push_main_encoders[i] = enc_obj
+
+        self.debug(self.__push_main_encoders)
+        self.parameter_remapped.replace_subjects(self.__push_main_encoders)
 
     def send_sysex(self, unique_portion):
         msg = unique_portion + (247,)
@@ -56,3 +70,8 @@ class Push1Display(ZCXPlugin):
         final = line_start + _bytes
 
         self.send_sysex(final)
+
+    @listens_group('mapped_parameter')
+    def parameter_remapped(self, enc_obj):
+        self.debug(f'{enc_obj} remapped: {enc_obj.mapped_parameter.name}')
+        self.debug(self.__push_main_encoders)
