55b384740828f5429b53beb7e52fde2537725b06
odisfm <102568741+odisfm@users.noreply.github.com>
2025-03-24 21:05:04 +1100

    build: force user to specify hardware to build for

--- tools/sync_config.py
+++ tools/sync_config.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 HOME = Path.home()
 CONTROL_SURFACE_NAME = "_zcx_core"
-HARDWARE_CONFIG = "push_1"
+HARDWARE_CONFIG = None
 
 REMOTE_SCRIPTS_PATH = HOME / "Music/Ableton/User Library/Remote Scripts"
 PROJECT_ROOT = Path(__file__).resolve().parent.parent
