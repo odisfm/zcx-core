ab4b690ee46ae174235c4bd17683f3bb63b2969f
odisfm <102568741+odisfm@users.noreply.github.com>
2025-03-25 19:39:21 +1100

    feat: harden yaml loader

--- app/yaml_loader.py
+++ app/yaml_loader.py
@@ -1,5 +1,4 @@
 import os
-
 from .vendor.yaml import safe_load
 
 
@@ -9,17 +8,24 @@ class YamlLoader:
         from . import ROOT_LOGGER
         self.__logger = ROOT_LOGGER.getChild(self.__class__.__name__)
         self.log('initialised')
-        self.this_dir = os.path.dirname(__file__)
+        self.__base_dir = os.path.abspath(os.path.dirname(__file__))
 
     def log(self, *msg):
-        for msg in msg:
-            self.__logger.info(msg)
+        for message in msg:
+            self.__logger.info(message)
 
     def load_yaml(self, path):
         if not path.endswith('.yaml'):
             raise ValueError('path must end with .yaml')
 
-        full_path = os.path.join(self.this_dir, path)
+        full_path = os.path.abspath(os.path.join(self.__base_dir, path))
+
+        # Check that the resulting path is within the allowed base directory.
+        if os.path.commonpath([full_path, self.__base_dir]) != self.__base_dir:
+            raise ValueError('Attempted directory traversal outside of base directory')
+
+        if not os.path.isfile(full_path):
+            raise FileNotFoundError(f'{full_path} does not exist or is not a file')
 
         with open(full_path, 'r') as f:
             obj = safe_load(f)
