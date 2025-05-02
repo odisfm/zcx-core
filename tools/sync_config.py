3f4e1fdbfe69b2eafecb95445867c0c67de6c14b
odisfm <102568741+odisfm@users.noreply.github.com>
2025-05-02 11:19:41 +1000

    build: add support for Windows users as well as `--user-library` switch to specify user library path

--- tools/sync_config.py
+++ tools/sync_config.py
@@ -4,6 +4,7 @@ HOME = Path.home()
 CONTROL_SURFACE_NAME = "_zcx_core"
 HARDWARE_CONFIG = None
 
-REMOTE_SCRIPTS_PATH = HOME / "Music/Ableton/User Library/Remote Scripts"
+REMOTE_SCRIPTS_PATH = None
+
 PROJECT_ROOT = Path(__file__).resolve().parent.parent
 SRC_ROOT = PROJECT_ROOT / "app"
