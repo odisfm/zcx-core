a7a31e29f4609f132f104b6d7366da44f5f33e89
odisfm <102568741+odisfm@users.noreply.github.com>
2025-09-13 08:17:45 +1000

    test: add tests

--- tests/test_pages.py
+++ tests/test_pages.py
@@ -12,7 +12,19 @@ class TestPages(unittest.TestCase):
 
     def setUp(self):
         self.page_manager: "PageManager" = self._zcx.component_map["PageManager"]
+        self.page_manager.set_page(page_number=0)
 
     def test_initial_page(self):
-        self.page_manager.set_page(page_number=0)
         self.assertEqual(self.page_manager.current_page, 0)
+
+    def test_page_increment(self):
+        page_num = self.page_manager.current_page
+        self.page_manager.increment_page(1)
+        self.assertEqual(self.page_manager.current_page, page_num + 1)
+        self.page_manager.increment_page(-1)
+        self.assertEqual(self.page_manager.current_page, page_num)
+
+    def test_navigate_to_page_name(self):
+        all_pages = self.page_manager.all_page_names
+        self.page_manager.set_page(page_name=all_pages[1])
+        self.assertEqual(self.page_manager.current_page, 1)
---
+++ tests/test_templates.py
@@ -0,0 +1,45 @@
+import unittest
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from zcx_core import ZCXCore
+    from page_manager import PageManager
+    from z_manager import ZManager
+    from pad_section import PadSection
+
+
+class TestTemplates(unittest.TestCase):
+
+    _zcx: "ZCXCore"
+
+    def setUp(self):
+        self.z_manager: "ZManager" = self._zcx.component_map["ZManager"]
+        self.test_section_1: "PadSection" = self.z_manager.get_matrix_section("test_section_1")
+
+    def test_global_template(self):
+        control = self.test_section_1.owned_controls[3]
+        self.assertTrue(control._context["me"]["props"]["global_template"])
+        self.assertEqual(control._on_threshold, 20)
+        self.assertEqual(control._color.midi_value, 127)
+
+    def test_stacked_template(self):
+        control = self.test_section_1.owned_controls[4]
+        self.assertEqual(control._color.midi_value, 3)
+        gestures = control._gesture_dict
+        self.assertEqual(gestures["pressed"], "control_def")
+        self.assertEqual(gestures["released"], "test_1")
+        self.assertEqual(gestures["released_delayed"], "control_def")
+        self.assertEqual(gestures["double_clicked"], "test_2")
+        self.assertTrue(control._context["me"]["props"]["test_1"])
+        self.assertTrue(control._context["me"]["props"]["test_2"])
+        self.assertTrue(control._context["me"]["props"]["control_def"])
+
+    def test_null_template(self):
+        control = self.test_section_1.owned_controls[5]
+        def get_test_prop(ctr):
+            return ctr._context["me"]["props"]["global_template"]
+        self.assertRaises(KeyError, lambda: get_test_prop(control))
+
+        control_2 = self.test_section_1.owned_controls[6]
+        self.assertRaises(KeyError, lambda: get_test_prop(control_2))
+        self.assertTrue(control_2._context["me"]["props"]["test_1"])
