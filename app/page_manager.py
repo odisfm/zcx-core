40e26712650cb8d5e825fd0cb9e1e996c8b0b1d7
odisfm <102568741+odisfm@users.noreply.github.com>
2025-09-22 08:11:32 +1000

    feat: add property `page_count`

--- app/page_manager.py
+++ app/page_manager.py
@@ -62,6 +62,10 @@ class PageManager(ZCXComponent):
     def all_page_names(self):
         return self.__page_names
 
+    @property
+    def page_count(self):
+        return self.__page_count
+
     def increment_page(self, increment=1):
         new_page = (self.__current_page + increment) % self.__page_count
         self.set_page(page_number=new_page)
